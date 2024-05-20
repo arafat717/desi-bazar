7ed7b067805de4bad4ae4ad4343a3dde9b6adeb9
arafat717 <arafatjibon33@gmail.com>
2024-05-20 21:54:13 +0600

    added initial setup

---
+++ src/routes/routes.jsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/Mainlayout/MainLayout";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
+import Contact from "../pages/Contact/Contact";
+import Login from "../pages/Login";
+import Resistration from "../pages/Resistration";
+import DashboardLayout from "../components/DashboardLayout/DashboardLayout";
+import Dashboard from "../components/DashboardLayout/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/resister",
+        element: <Resistration></Resistration>,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout></DashboardLayout>,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard></Dashboard>,
+      },
+    ],
+  },
+]);
+
+export default router;
