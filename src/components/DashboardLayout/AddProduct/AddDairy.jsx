6b6a02ea1e4e65df1d6d9e678488cdb209e5f444
arafat717 <arafatjibon33@gmail.com>
2024-07-13 13:10:14 +0600

    added image uploaded functionalities

--- src/components/DashboardLayout/AddProduct/AddDairy.jsx
+++ src/components/DashboardLayout/AddProduct/AddDairy.jsx
@@ -1,41 +1,63 @@
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+const image_hosting_token = import.meta.env.VITE_IMAGE;
 
 const AddDairy = () => {
   const navigate = useNavigate();
+  const image_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_token}`;
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm();
+  console.log(image_hosting_token);
 
   const onSubmit = async (data) => {
-    console.log(data);
-    await fetch("http://localhost:5000/product/", {
+    const formdata = new FormData();
+    formdata.append("image", data.image[0]);
+    fetch(image_hosting_url, {
       method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(data),
+      body: formdata,
     })
       .then((res) => res.json())
-      .then(() => {
-        toast.success("Product added successfuly");
-        reset();
-        if (data.catagory == "fruits") {
-          navigate("/dashboard/fruits-manage");
-        }
-        if (data.catagory == "vegitables") {
-          navigate("/dashboard/vegitables-manage");
-        }
-        if (data.catagory == "dairy") {
-          navigate("/dashboard/dairy-manage");
-        }
-        if (data.catagory == "meat") {
-          navigate("/dashboard/meat-manage");
-        }
+      .then((response) => {
+        const image_url = response.data.display_url;
+        console.log(image_url);
+        const { catagory, description, name, price } = data;
+        const newItem = {
+          name,
+          catagory,
+          description,
+          price,
+          image: image_url,
+        };
+        console.log(newItem);
+        fetch("http://localhost:5000/product/", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(newItem),
+        })
+          .then((res) => res.json())
+          .then(() => {
+            toast.success("Product added successfuly");
+            reset();
+            if (data.catagory == "fruits") {
+              navigate("/dashboard/fruits-manage");
+            }
+            if (data.catagory == "vegitables") {
+              navigate("/dashboard/vegitables-manage");
+            }
+            if (data.catagory == "dairy") {
+              navigate("/dashboard/dairy-manage");
+            }
+            if (data.catagory == "meat") {
+              navigate("/dashboard/meat-manage");
+            }
+          });
       });
   };
   return (
@@ -134,7 +156,7 @@ const AddDairy = () => {
           </label>
           <input
             id="image"
-            type="url"
+            type="file"
             {...register("image", { required: true })}
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
           />
